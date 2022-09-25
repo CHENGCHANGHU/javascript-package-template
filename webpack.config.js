4a4e48ff848d7e0003e8f46cbb28b01ad962fd76
CHENGCHANGHU <1669675499@qq.com>
2022-09-25 10:11:51 +0800

    feat: dev:windows

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,10 @@ console.log('mode:', process.env.mode);
 console.log('minimize:', process.env.minimize);
 
 function getWebpackConfig () {
-  console.log(process.env.mode, typeof process.env.mode, process.env.mode === 'production');
-  console.log(JSON.stringify(process.env.mode), JSON.stringify(process.env.mode) === 'production');
+  // console.log(process.env.mode, typeof process.env.mode, process.env.mode === 'production');
+  // console.log(JSON.stringify(process.env.mode), JSON.stringify(process.env.mode) === 'production');
   return {
-    mode: process.env.mode === 'development' ? 'production' : 'development',
+    mode: process.env.mode,
     optimization: {
       minimize: JSON.parse(process.env.minimize),
     },
